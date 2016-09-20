89c554dd91ae18a2e23623002b405321372bfc0c
alejo <alejodaraio@gmail.com>
2016-09-20 10:33:26 -0300

    added kaltura functions

--- src/ReactKalturaResumableJs.js
+++ src/ReactKalturaResumableJs.js
@@ -1,8 +1,34 @@
 import React from "react";
+import axios from "axios";
 
 export default class ReactKalturaResumableJs extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    _request(server, ks, path, queryString) {
+
+        let url = server + path + "?format=1&ks=" + ks + "&" + queryString;
+        let config = {
+            responseType: 'json'
+        };
+
+        return new Promise(function (fulfill, reject) {
+            axios.post(url, {}, config)
+                .then(fulfill)
+                .catch(reject);
+        });
+
+    }
+
+    _addMedia(server, ks, uploadToken, name) {
+        let self = this;
+        return new Promise(function (fulfill, reject) {
+            let path = "/service/media/action/addFromUploadedFile";
+            let queryString = "mediaEntry:name=" + encodeURIComponent(name) + "&mediaEntry:mediaType=1&uploadTokenId=" + uploadToken;
+
+            self._request(server, ks, path, queryString).then(fulfill).catch(reject);
+        })
     }
 
     render() {

8adb35abe2517dd92f102e72d64f5cbaad1d7237
alejo <alejodaraio@gmail.com>
2016-09-29 11:27:01 -0300

    updated package.json

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
+                exclude: /node_modules/,
                 query: {
                     cacheDirectory: true,
                     presets: ['react', 'es2015','stage-0']
